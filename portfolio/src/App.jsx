19612435afe6b94f5dc55c029a157e5690e7130c
Jonathan Dantas <jonathan.cs@hotmail.com>
2024-03-04 19:53:16 -0300

    Feat: adicionando html do header

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './componentes/header/header'
+
 import './App.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
---
+++ portfolio/src/componentes/header/header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <span>Portfólio</span>
+            <nav>
+                <li>Sobre Mim</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
