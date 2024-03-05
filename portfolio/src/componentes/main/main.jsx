de4d0ca2e58d6dd099c6ad3f0978807f043c6a9d
Jonathan Dantas <jonathan.cs@hotmail.com>
2024-03-05 16:07:16 -0300

    feat: Adicionando section "meus projetos"

--- portfolio/src/componentes/main/main.jsx
+++ portfolio/src/componentes/main/main.jsx
@@ -1,4 +1,5 @@
 import './main.css'
+import Projects from './projects'
 
 const Main = () => {
     return (
@@ -12,11 +13,17 @@ const Main = () => {
                     </article>
                     <article>
                         <h2>Qualificações</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam dolore esse voluptas natus deleniti enim magnam vel itaque quidem, facere sint illo alias sequi dolores delectus, maxime nam eum.</p>
+                        <p className='textp'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam dolore esse voluptas natus deleniti enim magnam vel itaque quidem, facere sint illo alias sequi dolores delectus, maxime nam eum.</p>
                     </article>
                 </div>
                 <div>
                     <img src="./src/assets/images/jonathan.png" alt="" />
+                </div>
+            </section>
+            <section>
+                <h1>Meus Projetos</h1>
+                <div className='projects'>
+                    <Projects/>
                 </div>
             </section>
         </div>
---
+++ portfolio/src/componentes/main/projects.jsx
@@ -0,0 +1,24 @@
+
+const Projects = () => {
+    return (
+        <>
+            <article>
+                <img src="../src/assets/images/projetos1.png" alt="Projeto Calculadora de IMC" />
+                <p>Calculadora IMC</p>
+                <button>Mais informações</button>
+            </article>
+            <article>
+                <img src="../src/assets/images/projetos2.png" alt="Projeto Tela de Login" />
+                <p>Tela de Login</p>
+                <button>Mais informações</button>
+            </article>
+            <article>
+                <img src="../src/assets/images/projetos3.png" alt="Projeto Kimetsu no Yaiba" />
+                <p>Landing Page</p>
+                <button>Mais informações</button>
+            </article>
+        </>
+    )
+}
+
+export default Projects;
