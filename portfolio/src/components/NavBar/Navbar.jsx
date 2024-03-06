732bfad572e5bf6ab9bc46f31403c03c6ded7684
Jonathan Dantas <jonathan.cs@hotmail.com>
2024-03-06 15:09:26 -0300

    feat: adicionando menu hamburguer

--- portfolio/src/components/NavBar/Navbar.jsx
+++ portfolio/src/components/NavBar/Navbar.jsx
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils"
+import { getImageUrl } from "../../utils"
 
-export const NavBar = () =>{
+export const NavBar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
-                <img className={styles.menuBtn} src={getImageUrl("images/menu.png")} alt="botão do menu" />
-                <ul className={styles.menuItems}>
+                <img
+                    className={styles.menuBtn}
+                    src={
+                        menuOpen
+                            ? getImageUrl("images/menuClose.png")
+                            : getImageUrl("images/menu.png")
+                    }
+                    alt="botão do menu"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
+                <ul
+                    className={
+                        `${styles.menuItems
+                        } ${menuOpen && styles.menuOpen
+                        }`
+                    }
+                    onClick={
+                        () => setMenuOpen(false)
+                    }
+                >
                     <li><a href="#about">Sobre Mim</a></li>
                     <li><a href="#experience">Experiência</a></li>
                     <li><a href="#projects">Projetos</a></li>
